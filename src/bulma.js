971350c349a1b8b17a2ab566c9e09b168fe1292a
Himanshu Jangid <iamahalofan@gmail.com>
2021-05-24 01:21:33 +0530

    Bulmajs navbar control improved

--- src/bulma.js
+++ src/bulma.js
@@ -8,13 +8,18 @@ var burger = document.getElementsByClassName("navbar-burger");
 // ignore other navbars if created by mistake or intensionally
 // it just accepts the top navbar or the first navbar located on the top of the page
 var navbar = document.getElementsByClassName("navbar-menu");
-burger[0].addEventListener("click", (e) => {
 
-    // toggler the class is-active class in navbar-menu
-    if (navbar[0].classList.contains("is-active")) {
-        navbar[0].classList.remove("is-active")
-    }else{
-        navbar[0].classList.add("is-active")
-    }
-})
+try{
+    burger[0].addEventListener("click", (e) => {
 
+        // toggler the class is-active class in navbar-menu
+        if (navbar[0].classList.contains("is-active")) {
+            navbar[0].classList.remove("is-active")
+        }else{
+            navbar[0].classList.add("is-active")
+        }
+    })
+}
+catch(err){
+    // pass
+}
