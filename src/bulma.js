f64d36e2ae9a1accc808bb006f7cf2b98eff9240
ankit <ac2000choudhary@gmail.com>
2021-05-24 19:38:33 +0530

    Commented the Bumla Modal JS

--- src/bulma.js
+++ src/bulma.js
@@ -24,18 +24,33 @@ catch (err) {
     // pass
 }
 
+/**-----------------------------------------------------------------------------------------*
+ *                  Bulma Modal Js Toggling Control                                         *
+ * ---------------------------------------------------------------------------------------**/
 
+
+
+// it selects all the modal buttons
 var btn = document.getElementsByClassName("modal-button");
+
 for (let i = 0; i < btn.length; i++) {
 
+    // select all the modals which are targeted by the modal button
     var modal = document.getElementById(btn[i].dataset.target);
     try {
+
+        // toggler the class is-active class in modal
         btn[i].addEventListener("click", (e) => {
             modal.classList.toggle("is-active")
         })
+
+        // toggler the class is-active class in modal to close the modal
         modal.firstElementChild.addEventListener("click", (e) => {
             modal.classList.toggle("is-active")
         })
+
+        // toggler the class is-active class in modal to close the modal
+        // all the close class in the model are selected
         for(let j=0; j<modal.getElementsByClassName('close').length; j++)
         {
             modal.getElementsByClassName('close')[j].addEventListener('click', (e) => {
